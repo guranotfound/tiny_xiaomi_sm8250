1af165c872dc1101ace65d3f34b785d83aa26103
GuraNotFound <nguyenvanchung940@outlook.com>
2026-07-08 00:31:12 +0700

    Enable KALLSYMS and integrate Baseband-Guard support

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -265,12 +265,14 @@ static int sel_mmap_handle_status(struct file *filp,
 			       size, vma->vm_page_prot);
 }
 
-static const struct file_operations sel_handle_status_ops = {
+const struct file_operations sel_handle_status_ops = {
 	.open		= sel_open_handle_status,
 	.read		= sel_read_handle_status,
 	.mmap		= sel_mmap_handle_status,
 	.llseek		= generic_file_llseek,
 };
+
+EXPORT_SYMBOL_GPL(sel_handle_status_ops);
 
 #ifdef CONFIG_SECURITY_SELINUX_DISABLE
 static ssize_t sel_write_disable(struct file *file, const char __user *buf,
@@ -777,7 +779,7 @@ static ssize_t sel_write_relabel(struct file *file, char *buf, size_t size);
 static ssize_t sel_write_user(struct file *file, char *buf, size_t size);
 static ssize_t sel_write_member(struct file *file, char *buf, size_t size);
 
-static ssize_t (*const write_op[])(struct file *, char *, size_t) = {
+ssize_t (*const write_op[])(struct file *, char *, size_t) = {
 	[SEL_ACCESS] = sel_write_access,
 	[SEL_CREATE] = sel_write_create,
 	[SEL_RELABEL] = sel_write_relabel,
@@ -2164,3 +2166,5 @@ void exit_sel_fs(void)
 	unregister_filesystem(&sel_fs_type);
 }
 #endif
+
+EXPORT_SYMBOL_GPL(write_op);
